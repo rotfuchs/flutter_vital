f16422388c5f76d6899b3d1e2c433b7ad77d9bbc
rotfuchs <silentdarko@gmail.com>
2019-05-14 22:11:30 +0200

    first try line chart

--- lib/gui/dashboard/charts/blood_pressure_line_chart.dart
+++ lib/gui/dashboard/charts/blood_pressure_line_chart.dart
@@ -1,18 +1,36 @@
-
-
 import 'package:flutter/widgets.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class BloodPressureLineChart extends StatefulWidget {
+  final List<charts.Series> seriesList;
+
+  BloodPressureLineChart(this.seriesList);
+
   @override
-  BloodPressureLineChartState createState() => BloodPressureLineChartState();
+  BloodPressureLineChartState createState() => BloodPressureLineChartState(this.seriesList);
 }
 
 class BloodPressureLineChartState extends State<BloodPressureLineChart> {
+  final List<charts.Series> seriesList;
+
+  BloodPressureLineChartState(this.seriesList);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return new charts.TimeSeriesChart(
+        seriesList,
+        animate: true,
+        defaultRenderer: new charts.LineRendererConfig(
+            includePoints: true),
+    );
   }
+}
+
+class PulseValue {
+  final int pulse;
+  final DateTime recordingDate;
+
+  PulseValue(this.recordingDate, this.pulse);
 }
 
 // https://google.github.io/charts/flutter/example/line_charts/dash_pattern
