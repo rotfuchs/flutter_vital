d674493fa1c3ffe23cd3257f40582404f0ee1333
rotfuchs <silentdarko@gmail.com>
2019-05-14 16:57:21 +0200

    init project data

---
+++ lib/gui/dashboard/charts/blood_pressure_line_chart.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/widgets.dart';
+
+class BloodPressureLineChart extends StatefulWidget {
+  @override
+  BloodPressureLineChartState createState() => BloodPressureLineChartState();
+}
+
+class BloodPressureLineChartState extends State<BloodPressureLineChart> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+// https://google.github.io/charts/flutter/example/line_charts/dash_pattern
---
+++ lib/gui/dashboard/dashboard.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/widgets.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  DashboardState createState() => DashboardState();
+}
+
+class DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
