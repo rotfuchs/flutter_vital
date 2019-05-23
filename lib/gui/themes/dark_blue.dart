f9709379bf8391b8424244d71b76727eec578b88
rotfuchs <silentdarko@gmail.com>
2019-05-23 18:17:36 +0200

    created first version of a dark blue theme

---
+++ lib/gui/themes/dark_blue.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+final ThemeData darkBlueTheme = new ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: DarkBlueThemeColors.black
+);
+
+class DarkBlueThemeColors {
+  static const _blackPrimaryValue = 0xFF000000;
+
+  static const MaterialColor black = const MaterialColor(
+    _blackPrimaryValue,
+    const <int, Color>{
+      50:  const Color(0xFFe0e0e0),
+      100: const Color(0xFFb3b3b3),
+      200: const Color(0xFF808080),
+      300: const Color(0xFF4d4d4d),
+      400: const Color(0xFF262626),
+      500: const Color(_blackPrimaryValue),
+      600: const Color(0xFF000000),
+      700: const Color(0xFF000000),
+      800: const Color(0xFF000000),
+      900: const Color(0xFF000000),
+    },
+  );
+}
